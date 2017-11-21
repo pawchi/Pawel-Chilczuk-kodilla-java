f35c1a36d3ecb7992db48b751b4d1b0825650bae
pawchi <pawchi@o2.pl>
2017-11-21 15:40:56 +0100

    Zad 6.6  - 21.11.2017

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeutifier.java
@@ -0,0 +1,9 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeutifier {
+
+    public void beutify(String a,PoemDecorator poemDecorator){
+        System.out.println(poemDecorator.decorate(a));
+
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.beautifier;
+import com.kodilla.stream.lambda.*;
+
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+import static jdk.nashorn.internal.objects.NativeString.trim;
+
+public class StreamMain {
+    public static void main(String[] args){
+        PoemBeutifier poemBeutifier = new PoemBeutifier();
+
+
+        poemBeutifier.beutify("Text to beautify",(a) ->"ABC"+a+"ABC" );
+        poemBeutifier.beutify("Text to uppercase",(a) -> toUpperCase(a) );
+        poemBeutifier.beutify("TexT to LOWERCASE",(a) -> toLowerCase(a) );
+        poemBeutifier.beutify("  Text to trim   ",(a) -> trim(a) );
+
+
+    }
+
+
+}
