94afcd8774c78cc7aac2bfef8775d4f9cb4d3a5e
pawchi <pawchi@o2.pl>
2017-12-07 16:32:40 +0100

    Zad 8.4  - 07.12.2017

---
+++ kodilla-exception/src/main/java/com/kodilla/good/patterns/challenges/StreamMain.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args){
+        MovieStore allTitles = new MovieStore();
+        String theResultStringAllTitles = allTitles.getMovies().entrySet().stream()
+                .map(Map.Entry::toString)
+                .collect(Collectors.joining("!"));
+
+        System.out.println(theResultStringAllTitles);
+    }
+}
